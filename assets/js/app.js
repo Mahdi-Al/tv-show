c39e67be0c9736c488fcbc01dcbb500113cedac4
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-07-21 15:16:26 +0330

    made Carusel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,33 @@
+const carouselItem = document.querySelectorAll(".carousel-item");
 const dataMain = [];
+const dataCarousel = [];
+const getData2 = async (url) => {
+  try {
+    const request = await fetch(url);
+    // console.log(request);
+    const data = await request.json();
+    // console.log(data);
+    dataCarousel.push(data);
+    const imhEle = document.createElement("img");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// carouselItem.forEach((item, i) => {
+//   const imgEleCarousel = document.createElement("img");
+//   console.log(dataCarousel[i]);
+//   // console.log(i);
+//   // imgEleCarousel.src = dataCarousel.image.medium;
+
+//   item.append(imgEleCarousel);
+// });
+
+console.log(dataCarousel);
+// const imgEleCarousel = dataCarousel.forEach((obj) => {
+//   imgEleCarousel.src = obj.image.medium;
+// });
+
 async function getData(url) {
   try {
     const request = await fetch(url);
@@ -11,7 +40,7 @@ async function getData(url) {
   }
 }
 
-for (let i = 0; i < 44; i += 2) {
+for (let i = 1; i < 44; i += 2) {
   //   console.log(i);
   getData(`https://api.tvmaze.com/shows/${i}`);
 }
@@ -20,15 +49,15 @@ for (let i = 0; i < 44; i += 2) {
 
 const bgImg = document.querySelector("#bg-img");
 const mainEle = document.querySelector("main");
-dataMain.forEach((card) => {
-  console.log(card);
-});
+// dataMain.forEach((card) => {
+//   console.log(card);
+// });
 // console.log(dataMain);
 async function getData(url) {
   try {
     const request = await fetch(url);
     const data = await request.json();
-    // const bgImg = document.getElementById('bgImg'); // assuming an element with id "bgImg"
+    //  const bgImg = document.getElementById('bgImg'); // assuming an element with id "bgImg"
 
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("relative", "h-[400px]", "w-[300px]", "rounded-md");
@@ -52,7 +81,7 @@ async function getData(url) {
     const contentDiv = document.createElement("div");
     contentDiv.classList.add("absolute", "bottom-4", "left-4", "text-left");
 
-    const h1 = document.createElement("h1");
+    const h1 = document.createElement("h2");
     h1.textContent = `${data.name}`;
     h1.classList.add(
       "text-lg",
@@ -68,7 +97,7 @@ async function getData(url) {
     p.classList.add("mt-2", "text-sm", "text-gray-300", "block");
     contentDiv.appendChild(p);
 
-    const button = document.createElement("button");
+    const button = document.createElement("a");
     button.textContent = `${data.rating.average}`;
     button.classList.add(
       "mt-2",
@@ -84,12 +113,28 @@ async function getData(url) {
     cardDiv.appendChild(contentDiv);
     mainEle.appendChild(cardDiv);
 
-    console.log(data);
+    // console.log(data);
   } catch (error) {
     console.log(error);
   }
 }
 for (let i = 1; i < 22; i++) {
-  console.log(i);
+  // console.log(i);
   getData(`https://api.tvmaze.com/shows/${i}`);
 }
+// bgImg.innerHTML +=
+const promises = [];
+for (let i = 40; i < 45; i++) {
+  promises.push(getData2(`https://api.tvmaze.com/shows/${i}`));
+}
+
+Promise.all(promises).then(() => {
+  carouselItem.forEach((item, i) => {
+    const imgEleCarousel = document.createElement("img");
+    imgEleCarousel.classList.add("w-full", "h-96", "object-cover");
+    imgEleCarousel.src = dataCarousel[i].image.medium;
+    // ...
+    item.append(imgEleCarousel);
+  });
+});
+//   item.append(imgEleCarousel);
